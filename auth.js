3f85c18efab6ded46960b56cb46e8f895722f1ce
amalalmamb76768430yz-alt <amalalmamb76768430yz@gmail.com>
2025-12-26 04:44:57 +0200

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,29 +1,25 @@
-// auth.js
+
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://tngvhezfvgqnouzoyggr.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuZ3ZoZXpmdmdxbm91em95Z2dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2Nzc3NTQsImV4cCI6MjA4MjI1Mzc1NH0.uUtcsfrid_QUZhiPELtgscQNJ4kl6l3n9RsH5kR2CvY";
+export const supabase = createClient(
+  "https://tngvhezfvgqnouzoyggr.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuZ3ZoZXpmdmdxbm91em95Z2dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2Nzc3NTQsImV4cCI6MjA4MjI1Mzc1NH0.uUtcsfrid_QUZhiPELtgscQNJ4kl6l3n9RsH5kR2CvY"
+);
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-// SIGN UP
 export async function signUp(phone, password, inviteCode) {
-    const { data, error } = await supabase.auth.signUp({
-        phone,
-        password
-    });
+  const { data, error } = await supabase.auth.signUp({
+    phone,
+    password
+  });
 
-    if (error) throw error;
+  if (error) throw error;
 
-    const userId = data.user.id;
+  await supabase.from("users").insert({
+    id: data.user.id,
+    phone: phone,
+    invite_code: inviteCode,
+    is_active: true
+  });
 
-    // insert into users table
-    await supabase.from("users").insert({
-        id: userId,
-        phone: phone,
-        invite_code: inviteCode,
-        is_active: true
-    });
-
-    return data;
+  return data.user;
 }
